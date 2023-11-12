66753d44ed8f1ea847594ab1cb8fa7926fb5ffb7
TamirKhalid <139135606+TamirKhalid@users.noreply.github.com>
2023-11-12 18:57:08 +0300

    Update get_flags.c by Morgan

--- get_flags.c
+++ get_flags.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+/********************** Checked By Morgan ***********************/
 /********************** Checked By Tamir ***********************/
 /**
  * Created by : Morgan
